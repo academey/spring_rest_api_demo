38bd96c1c1d60f1a83bcf0b5fcce350b6113878a
hyunjoon park <hjpark@nethru.co.kr>
2019-01-15 13:52:50 +0900

    Add TestDescription annotation

--- src/test/java/me/whiteship/springrestapidemo/events/EventControllerTests.java
+++ src/test/java/me/whiteship/springrestapidemo/events/EventControllerTests.java
@@ -1,6 +1,7 @@
 package me.whiteship.springrestapidemo.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.whiteship.springrestapidemo.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -35,6 +36,7 @@ public class EventControllerTests {
 	ObjectMapper objectMapper;
 
 	@Test
+	@TestDescription("정상적으로 이벤트를 생성하는 테스트")
 	public void createEvent() throws Exception {
 		EventDto event = EventDto.builder()
 				.name("Spring").description("REST API Development")
@@ -66,6 +68,7 @@ public class EventControllerTests {
 	}
 
 	@Test
+	@TestDescription("입력 받을 수 없는 값을 사용한 경우 발생하는 테스트")
 	public void createEvent_Bad_Request() throws Exception {
 		Event event = Event.builder().name("Spring").description("REST API Development")
 				.beginEnrollmentDateTime(LocalDateTime.of(2018, 12, 23, 14, 21))
@@ -94,6 +97,7 @@ public class EventControllerTests {
 	}
 
 	@Test
+	@TestDescription("입력 값이 비어있는 경우 발생하는 테스트")
 	public void createEvent_Bad_Request_empty_input() throws Exception {
 		EventDto eventDto = EventDto.builder().build();
 
@@ -106,6 +110,7 @@ public class EventControllerTests {
 	}
 
 	@Test
+	@TestDescription("입력 값이 잘못된 경우 발생하는 테스트")
 	public void createEvent_Bad_Request_wrong_input() throws Exception {
 		EventDto eventDto = EventDto.builder()
 				.name("Spring").description("REST API Development")
